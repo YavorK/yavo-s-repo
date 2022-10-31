678370a5c6618d46ffd9360bd60f3dc128b21708
Yavor Kirov <yavor99@gmail.com>
2022-10-31 12:32:07 +0200

    tadaa

--- model_frames/index.js
+++ model_frames/index.js
@@ -10,7 +10,7 @@ function convertMsFrameToModel(inputFrame, inputToModelMap, modelArrayLength) {
     for (let index = 0; index < inputToModelMapLength; index++) {
         if (inputToModelMap[index]) {
             const newPosition = inputToModelMap[index];
-            console.log('msInputFrame take value number ' + index + ' and put it in modelArray position ' + newPosition);
+            //console.log('msInputFrame take value number ' + index + ' and put it in modelArray position ' + newPosition);
             const msValue = inputFrame[index];
             modelArray[newPosition] = msValue;
         }
@@ -19,34 +19,87 @@ function convertMsFrameToModel(inputFrame, inputToModelMap, modelArrayLength) {
     return modelArray;
 }
 
-
-
 const data = require('./blendShapes.js');
 
-console.log('data[0][\'BlendShapes\'][0]');
+// console.log('data[0][\'BlendShapes\'][0]');
 const msInputFrame = data[0]['BlendShapes'][0];
-console.log(msInputFrame); // [0.171, 0.164, 0.038, ... ]
+// console.log(msInputFrame); // [0.171, 0.164, 0.038, ... ]
 
 // todo: maybe have different maps for different models?
 // ELEMENT FROM MicrosoftAPI array goes to ELEMENT POSITION in the modelArray
 const msToModelMap = {
     0 /*eyeBlinkLeft*/: 67,
     1 /*eyeLookDownLeft*/: 40,
-    //todo: ...fill in the rest...
+    2 /*eyeLookInLeft*/: 44,
+    3 /*eyeLookOutLeft*/: 46,
+    4 /*eyeLookUpLeft*/: 42,
+    5 /*eyeSquintLeft*/: 22,
+    6 /*eyeWideLeft*/: 24,
+    7 /*eyeBlinkRight*/: 68,
+    8 /*eyeLookDownRight*/: 41,
+    9 /*eyeLookInRight*/: 45,
+    10 /*eyeLookOutRight*/: 47,
+    11 /*eyeLookUpRight*/: 43,
+    12 /*eyeSquintRight*/: 23,
+    13 /*eyeWideRight*/: 25,
+    14 /*jawForward*/: 26,
+    15 /*jawLeft*/: 27,
+    16 /*jawRight*/: 28,
+    17 /*jawOpen*/: 51,
+    18 /*mouthClose*/: 52,
+    19 /*mouthFunnel*/: 53,
+    20 /*mouthPucker*/: 31,
+    21 /*mouthLeft*/: 38,
+    22 /*mouthRight*/: 39,
+    23 /*mouthSmileLeft*/: 64,
+    24 /*mouthSmileRight*/: 65,
+    25 /*mouthFrownLeft*/: 29,
+    26 /*mouthFrownRight */: 30,
+    27 /*mouthDimpleLeft*/: 54,
+    28 /*mouthDimpleRight*/: 55,
+    29 /*mouthStretchLeft*/: 56,
+    30 /*mouthStretchRight*/: 57,
+    31 /*mouthRollLower*/: 58,
+    32 /*mouthRollUpper*/: 59,
+    33 /*mouthShrugLower*/: 32,
+    34 /*mouthShrugUpper*/: 33,
+    35 /*mouthPressLeft*/: 62,
+    36 /*mouthPressRight*/: 61,
+    37 /*mouthLowerDownLeft*/: 36,
+    38 /*mouthLowerDownRight*/: 37,
+    39 /*mouthUpperUpLeft*/: 62,
+    40 /*mouthUpperUpRight */: 63,
+    41 /*browDownLeft*/: 17,
+    42 /*browDownRight*/: 18,
+    43 /*browInnerUp*/: 19,
+    44 /*browOuterUpLeft*/: 20,
+    45 /*browOuterUpRight*/: 21,
+    46 /*cheekPuff*/: 48,
+    47 /*cheekSquintLeft*/: 49,
+    48 /*cheekSquintRight*/: 50,
+    49 /*noseSneerLeft*/: 34,
+    50 /*noseSneerRight*/: 35,
+    51 /*tongueOut*/: 66,
 }
-const modelArrayLength = 72;
+const modelArrayLength = 71;
 const frameForModel = convertMsFrameToModel(msInputFrame, msToModelMap, modelArrayLength);
-console.log('Array for (Meta) model:')
-console.log(frameForModel);
+
+// console.table(frameForModel)
+
+// console.log('Array for (Meta) model:')
+// console.log(frameForModel);
+
+
 // benchmark
-// console.time('modelAllFrames');
-// const modelAllFrames = [];
-// for(const element of data) {
-//     for(const msFrame of element['BlendShapes']){
-//         const modelFrame = convertMsFrameToModel(msFrame, msToModelMap, modelArrayLength)
-//         modelAllFrames.push(modelFrame);
-//     }
-// }
-// console.timeEnd('modelAllFrames')
-// console.log('Model frames produced: '+modelAllFrames.length)
+console.time('model_make_frames_total_time');
+const modelAllFrames = [];
+for(const element of data) {
+    for(const msFrame of element['BlendShapes']){
+        const modelFrame = convertMsFrameToModel(msFrame, msToModelMap, modelArrayLength)
+        modelAllFrames.push(modelFrame);
+    }
+}
+console.timeEnd('model_make_frames_total_time')
+
+console.log('Model frames produced: '+modelAllFrames.length)
 
